c2ce8295a84dca314a87a706df6836776fe57a92
Sihyun Lee <leesh3597@gmail.com>
2024-02-26 15:26:37 +0900

    Initial Commit

---
+++ SwiftUIMemo/SwiftUIMemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIMemoApp.swift
+//  SwiftUIMemo
+//
+//  Created by 이시현 on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
