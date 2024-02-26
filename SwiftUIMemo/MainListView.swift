c408031f297bc7b71ddca065f9d1cde86dc533ef
Sihyun Lee <leesh3597@gmail.com>
2024-02-26 15:55:41 +0900

    step1

---
+++ SwiftUIMemo/MainListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by 이시현 on 2/26/24.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainListView()
+}
